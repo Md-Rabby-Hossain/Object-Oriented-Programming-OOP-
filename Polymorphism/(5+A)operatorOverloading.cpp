cd05f12c0a917d6a4ab3231858b390bd9c3f4d4a
Rabby034 <golam.rabby.1592@gmail.com>
2025-08-13 13:18:41 -0600

    Create (5+A)operatorOverloading.cpp

---
+++ Polymorphism/(5+A)operatorOverloading.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class overloading
+{
+    int real, img;
+public:
+    void input(int real, int img)
+    {
+        this->real=real;
+        this->img=img;
+    }
+    overloading operator+(overloading obj)
+    {
+        overloading t;
+        t.real = real + obj.real;
+        t.img = img + obj.img;
+        return t;
+    }
+
+    friend overloading operator+(int n, overloading a)
+    {
+        overloading t;
+        t.real = n + a.real;
+        t.img = a.img;
+        return t;
+    }
+
+    void output()
+    {
+        cout<< real << "+ i"<<img<<endl;
+    }
+
+};
+
+
+
+int main()
+{
+    overloading a,b;
+    a.input(4,3);
+    b.input(7,8);
+    overloading c;
+    c = 5+a;
+    c.output();
+    return 0;
+}
