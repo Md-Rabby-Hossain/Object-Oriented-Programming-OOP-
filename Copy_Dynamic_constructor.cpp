37e9d490da5791accc3415d62e6d3c7512e2ca8e
Rabby034 <golam.rabby.1592@gmail.com>
2025-09-03 21:35:04 -0600

    Update Copy_Dynamic_constructor.cpp

--- Copy_Dynamic_constructor.cpp
+++ Copy_Dynamic_constructor.cpp
@@ -20,6 +20,20 @@ public:
         }
     }
 
+
+  // Copy Constructor (Deep Copy)
+    number(const number &obj)
+    {
+        size = obj.size;
+        arr = new int[size];  // allocate new memory
+        for(int i=0; i<size; i++)
+        {
+            arr[i] = obj.arr[i];  // copy elements
+        }
+        cout<<"Copy constructor called!"<<endl;
+    }
+
+
     void display()
     {
         cout<< "Array elements: "<<endl;
